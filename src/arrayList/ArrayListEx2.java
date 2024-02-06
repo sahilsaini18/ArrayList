0ca613591c8730a56b56350df148f2ef9c53b20b
sahilsaini18 <157702038+sahilsaini18@users.noreply.github.com>
2024-02-06 14:40:15 +0530

    ArrayListEx2.java

--- src/arrayList/ArrayListEx2.java
+++ src/arrayList/ArrayListEx2.java
@@ -1,17 +1,22 @@
 package arrayList;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.logging.Logger;
 
 public class ArrayListEx2 {
+	private static final Logger log = Logger.getLogger(ArrayListEx2.class.getName());
+
 	public static void main(String[] args) {
 		ArrayList<String> list = new ArrayList<String>();
 		list.add("alpha");
 		list.add("beta");
 		list.add("gamma");
+
 		// Traversing the list through Iterator
 		Iterator<String> itr = list.iterator();
 		while (itr.hasNext()) {// check if iterator has the elements
-			System.out.println(itr.next());
+			log.info(itr.next());
 		}
 	}
 }
