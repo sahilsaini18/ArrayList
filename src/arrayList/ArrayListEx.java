7b8fa20923d7b8260b407ef9d5e2520127a6cd36
sahilsaini18 <157702038+sahilsaini18@users.noreply.github.com>
2024-02-06 14:39:08 +0530

    ArrayListEx.java

--- src/arrayList/ArrayListEx.java
+++ src/arrayList/ArrayListEx.java
@@ -1,17 +1,22 @@
 package arrayList;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.logging.Logger;
 
 /**
- * ArrayList uses dynamic array for storing the elements there is no size limit
- * It is like vector in c++
+ * ArrayList uses a dynamic array for storing the elements; there is no size
+ * limit. It is like a vector in C++.
  */
 public class ArrayListEx {
+	private static final Logger log = Logger.getLogger(ArrayListEx.class.getName());
+
 	public static void main(String[] args) {
 		ArrayList<String> list = new ArrayList<String>();
-//to add elements .add is used
+
+		// To add elements, .add() is used
 		list.add("Mango");
-		list.add("avacardo");
-		System.out.println(list);
+		list.add("Avocado");
+
+		log.info(list.toString());
 	}
 }
