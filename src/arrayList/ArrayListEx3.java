06da709b2eec40913571e7311bf005c409370c73
sahilsaini18 <157702038+sahilsaini18@users.noreply.github.com>
2024-02-06 14:41:22 +0530

    ArrayListEx3.java

--- src/arrayList/ArrayListEx3.java
+++ src/arrayList/ArrayListEx3.java
@@ -1,19 +1,22 @@
 package arrayList;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.logging.Logger;
 
 public class ArrayListEx3 {
+	private static final Logger log = Logger.getLogger(ArrayListEx3.class.getName());
+
 	/**
-	 * @param args
-	 *  Iterating in the array list using for each loop
+	 * @param args Iterating in the array list using for each loop
 	 */
 	public static void main(String[] args) {
 		ArrayList<String> list = new ArrayList<String>();
 
 		list.add("hello");
 		list.add("apple");
+
 		for (String a : list) {
-			System.out.println(a);
+			log.info(a);
 		}
 	}
 }
